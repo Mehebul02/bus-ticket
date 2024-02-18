41b3122563fe9c0e55fd08ca0e5072098f970b71
Mehebul02 <mehebul2123@gmail.com>
2024-02-18 13:37:45 +0600

    total section complite

--- js/index.js
+++ js/index.js
@@ -29,31 +29,46 @@ for (const btn of allBtn) {
         }
         
         const selectedSeatNumber = document.getElementById('selected-seat-number')
-//         const td1 = document.createElement('td');
-//   const td2 = document.createElement('td');
-//   const td3 = document.createElement('td');
-//   const newRow = document.createElement('td');
-//   newRow.appendChild(td1);
-//   newRow.appendChild(td2);
-//   newRow.appendChild(td3);
-//   selectedSeatNumber.appendChild(newRow)
-const li = document.createElement("li");
-const p1 = document.createElement("p");
-const p2 = document.createElement("p");
-const p3 = document.createElement("p");
-li.appendChild(p1);
-li.appendChild(p2);
-li.appendChild(p3);
-li.style.listStyle='none'
-selectedSeatNumber.appendChild(li);
-  const seatName = e.target.parentNode.innerText;
-    p1.innerText = seatName;
-    // 
-    p2.textContent = 'Economoy'
-    selectedSeatNumber.appendChild(p2)
+        const td1 = document.createElement('td');
+  const td2 = document.createElement('td');
+  const td3 = document.createElement('td');
+ 
+  
 
-   p3.textContent = '500'
-    selectedSeatNumber.appendChild(p3)
+  const newRow = document.createElement('tr');
+  newRow.appendChild(td1);
+  newRow.appendChild(td2);
+  newRow.appendChild(td3);
+
+  selectedSeatNumber.appendChild(newRow)
+  const seatName = e.target.parentNode.innerText;
+td1.innerText = seatName;
+td2.textContent = 'Economoy'
+   selectedSeatNumber.appendChild(td2)
+   td3.textContent = '500'
+     selectedSeatNumber.appendChild(td3)
+
+
+
+// const li = document.createElement("li");
+// const p1 = document.createElement("p");
+// const p2 = document.createElement("p");
+// const p3 = document.createElement("p");
+
+// li.appendChild(p1);
+// li.appendChild(p2);
+// li.appendChild(p3);
+
+// li.style.listStyle='none'
+// selectedSeatNumber.appendChild(li);
+//   const seatName = e.target.parentNode.innerText;
+//     p1.innerText = seatName;
+//     // 
+//     p2.textContent = 'Economoy'
+//     selectedSeatNumber.appendChild(p2)
+
+//    p3.textContent = '500'
+//     selectedSeatNumber.appendChild(p3)
 
 
 
