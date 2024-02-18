6f4f4a32557fe496d963ce6524204a5334d929e9
Mehebul02 <mehebul2123@gmail.com>
2024-02-18 13:17:38 +0600

    total section added

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
-// const allBtn = document.querySelectorAll(".seat_btn");
-// console.log(allBtn)
-// let totalSeat =40
-//  let count = 0;
-//  for (const btn of allBtn) {
-//    btn.addEventListener("click", function halnDle(e) {
-//      count +=1
-//     setInnerText('seat_count',count)
-//     totalSeat -=1
-//     setInnerText('total_seat',totalSeat)
-    
-    
-    
-
-//    } )};
 
 const allBtn = document.querySelectorAll(".seat_btn");
 let totalSeat = 40;
@@ -28,7 +13,9 @@ for (const btn of allBtn) {
             totalSeat--;
             setInnerText('total_seat', totalSeat);
             btn.style.background='green'
+            btn.removeEventListener("click", handleClick);
         } else if (btn.classList.contains('select')) {
+            
             btn.classList.remove('seat_btn');
             selectedCount++;
             setInnerText('seat_count', selectedCount);
@@ -40,41 +27,35 @@ for (const btn of allBtn) {
             alert('You have selected a total of 4 tickets');
             return 
         }
-        const selectedContainer = document.getElementById(
-            "selected-seat-number"
-          );
+        
         const selectedSeatNumber = document.getElementById('selected-seat-number')
-        console.log("jhkgdfg")
+//         const td1 = document.createElement('td');
+//   const td2 = document.createElement('td');
+//   const td3 = document.createElement('td');
+//   const newRow = document.createElement('td');
+//   newRow.appendChild(td1);
+//   newRow.appendChild(td2);
+//   newRow.appendChild(td3);
+//   selectedSeatNumber.appendChild(newRow)
+const li = document.createElement("li");
+const p1 = document.createElement("p");
+const p2 = document.createElement("p");
+const p3 = document.createElement("p");
+li.appendChild(p1);
+li.appendChild(p2);
+li.appendChild(p3);
+li.style.listStyle='none'
+selectedSeatNumber.appendChild(li);
+  const seatName = e.target.parentNode.innerText;
+    p1.innerText = seatName;
+    // 
+    p2.textContent = 'Economoy'
+    selectedSeatNumber.appendChild(p2)
+
+   p3.textContent = '500'
+    selectedSeatNumber.appendChild(p3)
+
+
+
     });
 }
-
-// const allBtn = document.querySelectorAll(".seat_btn");
-// let totalSeat = 40;
-// let selectedCount = 0;
-
-// for (const btn of allBtn) {
-//     btn.addEventListener("click", function handleClick(e) {
-//         if (!btn.classList.contains('selected') && selectedCount < 4) {
-//             btn.classList.add('selected');
-//             selectedCount++;
-//             setInnerText('seat_count', selectedCount);
-//             totalSeat--;
-//             setInnerText('total_seat', totalSeat);
-//             // বাটনের টেক্সট দাম হিসাবে নেওয়া যায়
-//             const seatNumber = btn.innerText;
-//             console.log('সিট নাম্বার:', seatNumber);
-//             // সিটের ক্লাস দাম হিসাবে নেওয়া যায়
-//             const seatClass = btn.getAttribute('ক্িবতু');
-//             console.log('সিট ক্লাস:', seatClass);
-//         } else if (btn.classList.contains('selected')) {
-//             btn.classList.remove('selected');
-//             selectedCount--;
-//             setInnerText('seat_count', selectedCount);
-//             totalSeat++;
-//             setInnerText('total_seat', totalSeat);
-//         } else {
-//             alert('আপনি মোট ৪ টি টিকেট নির্বাচন করেছেন');
-//             return;
-//         }
-//     });
-// }
